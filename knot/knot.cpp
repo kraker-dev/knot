79c7fa5d798c3401a37297561cdd28af2c094b97
Sidhin S Thomas <sidhin.thomas@gmail.com>
2020-10-02 23:18:48 +0530

    Initial Commit. XAML Island working. (#8)
    Initial Commit. XAML Island working.

---
+++ knot/knot.cpp
@@ -0,0 +1,158 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.system.h>
+#include <winrt/windows.ui.xaml.hosting.h>
+#include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
+#include <winrt/windows.ui.xaml.controls.h>
+#include <winrt/Windows.ui.xaml.media.h>
+
+using namespace winrt;
+using namespace Windows::UI;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Xaml::Hosting;
+using namespace Windows::Foundation::Numerics;
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND _hWnd;
+HWND _childhWnd;
+// This HWND will be the window handler for the XAML Island: A child window that contains XAML.  
+HWND hWndXamlIsland = nullptr;
+HINSTANCE _hInstance;
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    _hInstance = hInstance;
+
+    // The main window class name.
+    const wchar_t windowClassName[] = L"KnotDesktopApp";
+    WNDCLASSEX windowClass = { };
+
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = hInstance;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+    windowClass.hIconSm = LoadIcon(windowClass.hInstance, IDI_APPLICATION);
+
+    if (RegisterClassEx(&windowClass) == NULL)
+    {
+        MessageBox(NULL, L"Windows registration failed!", L"Error", NULL);
+        return 0;
+    }
+
+    _hWnd = CreateWindow(
+        windowClassName,
+        L"Windows c++ Win32 Desktop App",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+    if (_hWnd == NULL)
+    {
+        MessageBox(NULL, L"Call to CreateWindow failed!", L"Error", NULL);
+        return 0;
+    }
+
+
+    // Begin XAML Island section.
+
+    // The call to winrt::init_apartment initializes COM; by default, in a multithreaded apartment.
+    winrt::init_apartment(apartment_type::single_threaded);
+
+    // Initialize the XAML framework's core window for the current thread.
+    WindowsXamlManager winxamlmanager = WindowsXamlManager::InitializeForCurrentThread();
+
+    // This DesktopWindowXamlSource is the object that enables a non-UWP desktop application 
+    // to host UWP controls in any UI element that is associated with a window handle (HWND).
+    DesktopWindowXamlSource desktopSource;
+
+    // Get handle to the core window.
+    auto interop = desktopSource.as<IDesktopWindowXamlSourceNative>();
+
+    // Parent the DesktopWindowXamlSource object to the current window.
+    check_hresult(interop->AttachToWindow(_hWnd));
+
+    // Get the new child window's HWND. 
+    interop->get_WindowHandle(&hWndXamlIsland);
+
+    // Update the XAML Island window size because initially it is 0,0.
+    RECT rect = {};
+    GetClientRect(_hWnd, &rect);
+    SetWindowPos(hWndXamlIsland, 0, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, SWP_SHOWWINDOW);
+
+    // Create the XAML content.
+    Windows::UI::Xaml::Controls::StackPanel xamlContainer;
+    xamlContainer.Background(Windows::UI::Xaml::Media::SolidColorBrush{ Windows::UI::Colors::DimGray() });
+
+    Windows::UI::Xaml::Controls::TextBox tb;
+    tb.Text(L"Hello World from Xaml Islands!");
+    tb.VerticalAlignment(Windows::UI::Xaml::VerticalAlignment::Center);
+    tb.HorizontalAlignment(Windows::UI::Xaml::HorizontalAlignment::Center);
+
+    xamlContainer.Children().Append(tb);
+    xamlContainer.UpdateLayout();
+    desktopSource.Content(xamlContainer);
+
+    // End XAML Island section.
+
+    ShowWindow(_hWnd, nCmdShow);
+    UpdateWindow(_hWnd);
+
+    //Message loop:
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT messageCode, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    RECT rcClient;
+
+    switch (messageCode)
+    {
+    case WM_PAINT:
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+        // Create main window
+    case WM_CREATE:
+        return 0;
+
+        // Main window changed size
+    case WM_SIZE:
+        // Get the dimensions of the main window's client
+        // area, and enumerate the child windows. Pass the
+        // dimensions to the child windows during enumeration.
+        GetClientRect(hWnd, &rcClient);
+        SetWindowPos(hWndXamlIsland, nullptr, rcClient.left, rcClient.top, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top, SWP_NOZORDER);
+        MoveWindow(_childhWnd, 200, 200, 400, 500, TRUE);
+        ShowWindow(_childhWnd, SW_SHOW);
+
+        return 0;
+
+        // Process other messages.
+
+    default:
+        return DefWindowProc(hWnd, messageCode, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
